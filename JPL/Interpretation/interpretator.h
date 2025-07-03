b4283c6288d7cca2ea9b0f7501b63075804c072d
MiLDeV <149592908+MilDevelop@users.noreply.github.com>
2025-07-03 10:21:07 +0700

    Update interpretator.h

--- JPL/Interpretation/interpretator.h
+++ JPL/Interpretation/interpretator.h
@@ -8,11 +8,11 @@ template <class Type>
 class Interp {
 public:
   static Type data; 
-  static int PC;
-  static int AC;
-  static string instr;
-  static int instr_type;
-  static vector<Type> data_loc;
+  static int PC; ////PC содержит адрес следующей команды
+  static int AC;// аккумулятор, регистр для арифметики
+  static string instr; //регистр для текущей команды
+  static int instr_type; //тип команды (код операции)
+  static vector<Type> data_loc; //адрес данных или -1 , если его нет
   static bool run_bit; //while we don't meet #END
   static int get_instr_type(string comm); //We'll get data-index our command
   static vector<Type> find_data(static string instr, int type); //first value is mean that command need agrs or needn't
@@ -21,6 +21,14 @@ public:
   static void execute(int type); //execute command without args
   static void execute(int type, Type data, int Type_of_DataArgs); //execute command with args
   static void interpret(vector<string> memory, int starting_address); //main loop with interpetation JPL-code
+//Эта процедура интепретирует программы для простой машины.
+// которая содержит команды только с одним операндом из памяти Машина содержит регистр АС
+// (аккумулятор) Он используется для арифметических действий Например, команда ADD суммирует
+// число из памяти с АС. Интерпретатор работает до тех пор. пока не будет выполнена команда
+// END, вследствие чего бит run_bit поменяет значение на false. Машина состоит из памяти,
+// счетчика команд, бита run bit и аккумулятора АС Входные параметры состоят из копии
+// содержимого памяти и начального адреса
+
 };
 
 #endif
