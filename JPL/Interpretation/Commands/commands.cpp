bce5011237ffb29041339ae9f7258990b1b128fe
MiLDeV <149592908+MilDevelop@users.noreply.github.com>
2025-07-03 10:16:50 +0700

    Update commands.cpp

--- JPL/Interpretation/Commands/commands.cpp
+++ JPL/Interpretation/Commands/commands.cpp
@@ -4,6 +4,6 @@
 const vector<map<string, int>> command_keys = {
   {"ConsoleOUT" : 1},
   {"int" : 2}, {"str" : 3}, {"double" : 4}, {"bool" : 5},
-  {"+" : 6}, {"-" : 7}, {"*" : 8}, {"/" : 9}
+  {"ADD" : 6}
 }
 
